92a5aef1cc92497241b30cd22e1bf9cfb20f3575
N1ebieski <mariusz.wysokinski@neostrada.pl>
2021-03-01 17:39:12 +0100

    additional disks

--- config/file-manager.php
+++ config/file-manager.php
@@ -31,7 +31,7 @@ return [
      * List of disk names that you want to use
      * (from config/filesystems)
      */
-    'diskList' => ['public', 'views', 'css', 'lang'],
+    'diskList' => ['public', 'views', 'lang', 'css', 'js', 'images', 'svg'],
 
     /**
      * Default disk for left manager
